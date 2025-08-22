904caf31208d0ecd54bf74b5587cc37cc5bd44b8
Simon Wegscheider <simonwegscheider@MacBookAir.fritz.box>
2025-08-22 13:43:25 +0200

    Änderungen wie in Aufgabenstellung

--- my_source.py
+++ my_source.py
@@ -1,1 +1,13 @@
-print("Hello World")
+"""Contains code under test for solution_ci."""
+
+def add_func(a, var_two):
+    """
+    Add two variables.
+
+    Args
+        a: first variable
+        var_two: second variable
+
+    """
+    return a + var_two
+
